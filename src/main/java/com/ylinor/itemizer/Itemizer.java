3704d1c23aabdd2472040327c9435f6f7c4c4b0d
Zessirb <zessirb@gmail.com>
2017-10-20 13:42:40 +0200

    Created configuration handler

--- src/main/java/com/ylinor/itemizer/Itemizer.java
+++ src/main/java/com/ylinor/itemizer/Itemizer.java
@@ -1,32 +1,41 @@
 package com.ylinor.itemizer;
 
+import com.ylinor.itemizer.data.handlers.ConfigurationHandler;
+import com.ylinor.itemizer.utils.ItemBuilder;
 import ninja.leaping.configurate.ConfigurationNode;
 import ninja.leaping.configurate.commented.CommentedConfigurationNode;
 import ninja.leaping.configurate.loader.ConfigurationLoader;
 import org.slf4j.Logger;
+import org.spongepowered.api.config.ConfigDir;
 import org.spongepowered.api.config.DefaultConfig;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.game.state.GameStartedServerEvent;
 import org.spongepowered.api.plugin.Plugin;
 
 import javax.inject.Inject;
+import java.nio.file.Path;
+import java.util.Arrays;
 
 @Plugin(id = "itemizer", name = "Itemizer", version = "0.0.1")
 public class Itemizer {
 
 	@Inject
-	private Logger logger;
+	@ConfigDir(sharedRoot=true)
+	private Path configDir;
 
+	private static Logger logger;
 	@Inject
-	@DefaultConfig(sharedRoot = true)
-	private ConfigurationLoader<CommentedConfigurationNode> configurationLoader;
+	private void setLogger(Logger logger) {
+		this.logger = logger;
+	}
+	public static Logger getLogger() {
+		return logger;
+	}
 
 	@Listener
 	public void onServerStart(GameStartedServerEvent event) throws Exception {
-		logger.info(" [Itemizer] Enabling Itemizer...");
-
-		ConfigurationNode rootNode = configurationLoader.load();
-		//System.out.println(rootNode.getNode("itemizer", "toto", "aze").getInt());
+		ConfigurationHandler.readMinerConfiguration(ConfigurationHandler.loadConfiguration(configDir+"/itemizer_miner.conf"));
+		logger.info("ITEMIZER initialized.");
 	}
 
 }
---
+++ src/main/java/com/ylinor/itemizer/data/handlers/ConfigurationHandler.java
@@ -0,0 +1,58 @@
+package com.ylinor.itemizer.data.handlers;
+
+import com.google.common.reflect.TypeToken;
+import com.ylinor.itemizer.Itemizer;
+import com.ylinor.itemizer.data.beans.MinerBean;
+import com.ylinor.itemizer.data.serializers.MinerSerializer;
+import ninja.leaping.configurate.commented.CommentedConfigurationNode;
+import ninja.leaping.configurate.hocon.HoconConfigurationLoader;
+import ninja.leaping.configurate.loader.ConfigurationLoader;
+import ninja.leaping.configurate.objectmapping.ObjectMappingException;
+import ninja.leaping.configurate.objectmapping.serialize.TypeSerializers;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConfigurationHandler {
+    private ConfigurationHandler() {}
+
+    private static List<MinerBean> minerList;
+    public static List<MinerBean> getMinerList(){
+        return minerList;
+    }
+
+    /**
+     * Read configuration and interpret it
+     * @param configurationNode ConfigurationNode to read from
+     */
+    public static void readMinerConfiguration(CommentedConfigurationNode configurationNode){
+        minerList = new ArrayList<>();
+        TypeSerializers.getDefaultSerializers().registerType(TypeToken.of(MinerBean.class), new MinerSerializer());
+        try {
+            minerList = configurationNode.getNode("miners").getList(TypeToken.of(MinerBean.class));
+            for (MinerBean miner: minerList) {
+                Itemizer.getLogger().debug("Miner from config : " + miner.getId());
+            }
+        } catch (ObjectMappingException e) {
+            Itemizer.getLogger().error("Error while reading configuration 'harvestables' : " + e.getMessage());
+        }
+    }
+
+    /**
+     * Load configuration from file
+     * @param configName Name of the configuration in the configuration folder
+     * @return Configuration ready to be used
+     */
+    public static CommentedConfigurationNode loadConfiguration(String configName) {
+        ConfigurationLoader<CommentedConfigurationNode> configLoader = HoconConfigurationLoader.builder().setPath(Paths.get(configName)).build();
+        CommentedConfigurationNode configNode = null;
+        try {
+            configNode = configLoader.load();
+        } catch (IOException e) {
+            Itemizer.getLogger().error("Error while loading configuration '" + configName + "' : " + e.getMessage());
+        }
+        return configNode;
+    }
+}
