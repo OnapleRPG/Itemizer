f79a354db38bee6088de6273ed48ad6dd9054e2d
NeumimTo <chleba48@gmail.com>
2019-03-24 20:28:07 +0100

    ingredient adapter

--- src/main/java/com/onaple/itemizer/data/serializers/IngredientMapAdapter.java
+++ src/main/java/com/onaple/itemizer/data/serializers/IngredientMapAdapter.java
@@ -5,20 +5,28 @@ import ninja.leaping.configurate.ConfigurationNode;
 import ninja.leaping.configurate.objectmapping.ObjectMappingException;
 import ninja.leaping.configurate.objectmapping.serialize.TypeSerializer;
 import org.checkerframework.checker.nullness.qual.Nullable;
+import org.spongepowered.api.item.inventory.ItemStack;
 import org.spongepowered.api.item.recipe.crafting.Ingredient;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
  * Created by NeumimTo on 24.3.2019.
  */
-public class IngredientMapAdapter implements TypeSerializer<Map<Character,Ingredient>> {
+public class IngredientMapAdapter implements TypeSerializer<Map<Character, Ingredient>> {
 
     @Override
     public Map<Character, Ingredient> deserialize(TypeToken<?> type, ConfigurationNode value) throws ObjectMappingException {
         Map<Object, ? extends ConfigurationNode> childrenMap = value.getChildrenMap();
-        //todo
-        return null;
+        Map<Character, Ingredient> map = new HashMap<>();
+        for (Map.Entry<Object, ? extends ConfigurationNode> entry : childrenMap.entrySet()) {
+            String s = entry.getKey().toString();
+            ConfigurationNode node = entry.getValue();
+            ItemStack item = new ItemBeanRefOrItemIdAdapter().deserialize(type, node.getNode("item"));
+            map.put(s.charAt(0), Ingredient.builder().with(item).build());
+        }
+        return map;
     }
 
     @Override
