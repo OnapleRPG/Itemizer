3f0fcda85128f96a987e0d98f8640efce4f35ee2
zessirb <zessirb@gmail.com>
2020-03-02 14:49:27 +0100

    Cleaning up code

--- scripts/deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-ssh aventurier@onaple.fr "rm ~/sponge-server-1.12/mods/Itemizer*.jar"
-rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/build/libs/* aventurier@onaple.fr:~/sponge-server-1.12/mods/
--- server.sh
+++
@@ -1,4 +0,0 @@
-cd /home/hugo/Documents/Sponge/server\ 1.12/
-rm mods/Itemizer*
-mv ~/IdeaProjects/perso/Itemizer/build/libs/* /home/hugo/Documents/Sponge/server\ 1.12/mods/
-./run.sh
